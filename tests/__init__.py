5a31041e11b2774087a73b7653780f75a4f0b613
Patricie Hermanová <patriciehermanova@gmail.com>
2026-04-08 14:21:07 +0200

    Update docstring and import statements in __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
-"""Test package for quiz_common."""
+"""Shared data models used by quiz components."""
+
+from quiz_common.models import Option, Question, Quiz
+
+__all__ = ["Option", "Question", "Quiz"]
